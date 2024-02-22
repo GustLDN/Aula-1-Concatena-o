8e3648790602237c66b097f1393ef51e0abaa6b6
GustLDN <128159931+GustLDN@users.noreply.github.com>
2024-02-22 09:51:37 -0300

    Add files via upload

---
+++ type number/type-string.js
@@ -0,0 +1,10 @@
+const texto1 = "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+const senha = "senhaSuperSegura456";
+const stringDeNúmeros = "34567";
+const meuNome = "Gustavo"
+const citacao = 'O Leo disse "oi!"';
+console.log(citacao)
+// concatenação (+)
+console.log(citacao + meuNome)
+
